0f32d0954dd10ddf0357347b71a3490df3204f90
s140067 <m.j.jongeneel@tue.nl>
2023-10-17 14:03:17 +0200

    Added moving conveyor possibility

--- Functions/BoxSimulator.m
+++ Functions/BoxSimulator.m
@@ -1,4 +1,4 @@
-function [AH_B, BV_AB, FN, FT] = BoxSimulator(releasePosition,releaseOrientation,releaseLinVel,releaseAngVel,eN,eT,mu,box,AR_C,Ao_C,dt,endtime)
+function [AH_B, BV_AB, FN, FT] = BoxSimulator(x,c,box)
 %% Box-simulator-FixedPoint:
 %This script uses an augmented Lagrangian approach (fixed-point iteration)
 %for solving the nonlinear algebraic equations for contact impact and
@@ -7,22 +7,25 @@ function [AH_B, BV_AB, FN, FT] = BoxSimulator(releasePosition,releaseOrientation
 %we express all positions and velocities in terms of frame A, located at
 %the camera coordinate frame of a camera, also plotted in the figure.
 %
-% INPUTS:    releasePosition     : 3x1 double, position of the box at release
-%            releaseOrientation  : 3x3 double, orientation of the box at release
-%            releaseLinVel       : 3x1 double, linear velocity of the box at release
-%            releaseAngVel       : 3x1 double, angular velocity of the box at release
-%            eN                  : 1x1 double, normal coefficient of restitution
-%            eT                  : 1x1 double, tangential coefficient of restitution
-%            mu                  : 1x1 double, coefficient of friction
-%            box                : struct, with fields of box properties as
-%                                  box.B_M_B  : 6x6 double intertia tensor of the box
-%                                  box.mass    : 1x1 double mass of the box
-%                                  box.vertices: 3x8 double position of the vertices of 
+% INPUTS:    x.releasePosition   : 3x1 double, position of the box at release           [m]
+%            x.releaseOrientation: 3x3 double, orientation of the box at release        [deg]
+%            x.releaseLinVel     : 3x1 double, linear velocity of the box at release    [m/s]
+%            x.releaseAngVel     : 3x1 double, angular velocity of the box at release   [rad/s]
+%            c.eN                : 1x1 double, normal coefficient of restitution        [-]
+%            c.eT                : 1x1 double, tangential coefficient of restitution    [-]
+%            c.mu                : 1x1 double, coefficient of friction                  [-]
+%            c.AR_C              : 3x3 double, Orientation of the contact surface       [rad]
+%            c.Ao_C              : 3x1 double, Position of the contact surface          [m]
+%            c.Cv_AC             : 3x1 double, Lin velocity of the contact surface      [m/s]
+%            c.dt                : 1x1 double, Timestep at which the simulator runs     [1/s]
+%            c.endtime           : 1x1 double, Simulation time you want to run          [s]
+%            c.a                 : 1x1 double, Prox point auxilary parameter            [-]
+%            c.tol               : 1x1 double, Error tol for fixed-point                [-]
+%            box                 : struct, with fields of box properties as
+%                                  box.B_M_B  : 6x6 double intertia tensor of the box   []  
+%                                  box.mass    : 1x1 double mass of the box             [kg]
+%                                  box.vertices: 3x8 double position of the vertices of [m]
 %                                                the box w.r.t body-fixed frame
-%            AR_C                : 3x3 double, Orientation of the contact surface
-%            Ao_C                : 3x1 double, Position of the contact surface
-%            dt                  : 1x1 double, Timestep at which the simulator runs
-%            endtime             : 1x1 double, Simulation time you want to run
 %
 % OUTPUTS:   AH_B                : Pose of the box over time
 %            BV_AB               : Left trivialized velocity of the box over time
@@ -30,30 +33,19 @@ function [AH_B, BV_AB, FN, FT] = BoxSimulator(releasePosition,releaseOrientation
 %            FT                  : Tangential force acting on the box over time
 %% Constants and settings
 g     = 9.81;              %Gravitational acceleration              [m/s^2]
-a     = 0.01;              %Prox point auxilary parameter           [-]
-tol   = 1e-9;              %Error tol for fixed-point               [-]
-Cz_C  = [0;0;1];           %z-component of the C frame              [-]
-Cy_C  = [0;1;0];           %y-component of the C frame              [-]
-Cx_C  = [1;0;0];           %x-component of the C frame              [-]
-Bv_AB = releaseLinVel;     %Linear velocity at t0 of B wrt frame A  [m/s]
-Bomg_AB = releaseAngVel;   %Angular velocity at t0 of B wrt frame A [m/s]
+Bv_AB = x.releaseLinVel;     %Linear velocity at t0 of B wrt frame A  [m/s]
+Bomg_AB = x.releaseAngVel;   %Angular velocity at t0 of B wrt frame A [m/s]
 PNfull = zeros(8,1);       %Initial guess for momenta PN            [N*s]
 PTfull(1:8,1) = {[0;0]};   %initial guess for momenta PT            [N*s]
-run    = true;             %Boolean to determine if box is moving   [-]
-endframe = endtime/dt;     %Run to this frame                       [-]
+N = c.endtime/c.dt;     %Run to this frame                       [-]
 
 %% Preallocate memory to speed up the process
-FT = NaN(8,endframe);
-FN = NaN(4,endframe); 
-AH_B = cell(1,endframe);
-E  = NaN(1,endframe);
+FT = NaN(8,N);
+FN = NaN(4,N); 
+AH_B = cell(1,N);
+E  = NaN(1,N);
 
 %% Retrieve info of box struct
-Bp_1 = box.vertices(:,1); Bp_5 = box.vertices(:,5);
-Bp_2 = box.vertices(:,2); Bp_6 = box.vertices(:,6);
-Bp_3 = box.vertices(:,3); Bp_7 = box.vertices(:,7);
-Bp_4 = box.vertices(:,4); Bp_8 = box.vertices(:,8);
-
 B_M_B = box.B_M_B;
                            
 %% Wrench acting on body B: expressed in B with orientation of A:
@@ -63,46 +55,30 @@ BA_f   = [BA_fo; BA_Tau];
 
 %% Initial pose and velocity
 %Initial pose of the box: Transformation matrix B w.r.t. frame A
-AR_B    = releaseOrientation;%Initial orientation
-Ao_B    = releasePosition;   %Initial position
+AR_B    = x.releaseOrientation;%Initial orientation
+Ao_B    = x.releasePosition;   %Initial position
 AH_B{1} = [AR_B, Ao_B; zeros(1,3), 1]; %Homogeneous transformation matrix
 
 %Initial left trivialized velocity of frame B w.r.t. frame A
 BV_AB(:,1) = [Bv_AB; Bomg_AB];
 
-%% Defining the contact plane
-%Direction of the normal and in-plane vectors of the plane w.r.t. frame A
-Az_C = AR_C*Cz_C;
-Ay_C = AR_C*Cy_C;
-Ax_C = AR_C*Cx_C;
-
 %% Dynamics
-ii = 1;
-while run 
+
+for ii=1:N
     %Rewrite the body velocity as a 4x4 matrix in se(3)
     BV_ABs = hat(BV_AB(:,ii));
     
     %Kinematics: Compute the configuration at the mid-time (tM)
-    AH_Bm = AH_B{ii}*expm(0.5*dt*BV_ABs);
+    AH_Bm = AH_B{ii}*expm(0.5*c.dt*BV_ABs);
     AR_Bm = AH_Bm(1:3,1:3);
     Ao_Bm = AH_Bm(1:3,4);
     AR_B  = AH_B{ii}(1:3,1:3);
     
     %Compute the wrench at tM
     B_fM = ([AR_Bm zeros(3); zeros(3) AR_Bm])'*BA_f;
-    
-    %And compute the gap-functions at tM
-    gNm1 = Az_C'*((Ao_Bm + AR_Bm*Bp_1)-Ao_C);
-    gNm2 = Az_C'*((Ao_Bm + AR_Bm*Bp_2)-Ao_C);
-    gNm3 = Az_C'*((Ao_Bm + AR_Bm*Bp_3)-Ao_C);
-    gNm4 = Az_C'*((Ao_Bm + AR_Bm*Bp_4)-Ao_C);
-    gNm5 = Az_C'*((Ao_Bm + AR_Bm*Bp_5)-Ao_C);
-    gNm6 = Az_C'*((Ao_Bm + AR_Bm*Bp_6)-Ao_C);
-    gNm7 = Az_C'*((Ao_Bm + AR_Bm*Bp_7)-Ao_C);
-    gNm8 = Az_C'*((Ao_Bm + AR_Bm*Bp_8)-Ao_C);
-    
-    %column of normal contact distances
-    gN = [gNm1;gNm2;gNm3;gNm4;gNm5;gNm6;gNm7;gNm8];
+
+    %And compute the gap-functions at tM column of normal contact distances
+    gN = (c.AR_C(:,3)'*(Ao_Bm + AR_Bm*box.vertices-c.Ao_C))';
     
     %Obtain the linear and angular velocity at tA
     vA = BV_AB(:,ii);
@@ -110,55 +86,47 @@ while run
     %If a gap function has been closed, contact has been made
     IN = find(gN<0);
     if  IN > 0
-        %Compute the matrix containing the normal force directions at tA and tM
-        WNta = CompWN(AR_B,Az_C,Bp_1,Bp_2,Bp_3,Bp_4,Bp_5,Bp_6,Bp_7,Bp_8);
-        WNtm = CompWN(AR_Bm,Az_C,Bp_1,Bp_2,Bp_3,Bp_4,Bp_5,Bp_6,Bp_7,Bp_8);
-        %Compute the matrix containing the tangential force directions at tA and tM
-        WTta = CompWT(AR_B,Ax_C,Ay_C,Bp_1,Bp_2,Bp_3,Bp_4,Bp_5,Bp_6,Bp_7,Bp_8);
-        WTtm = CompWT(AR_Bm,Ax_C,Ay_C,Bp_1,Bp_2,Bp_3,Bp_4,Bp_5,Bp_6,Bp_7,Bp_8);
-        
-        %Matrix with force directions at tA and tM
-        WNA = WNta(:,IN);
-        WNM = WNtm(:,IN);
-        WTA = cell2mat(WTta(:,IN));
-        WTM = cell2mat(WTtm(:,IN));
+        %Compute the matrix containing force directions
+        [WNA, WTA] = CompW(AR_B,c.AR_C,box.vertices(:,IN));
+        [WNM, WTM] = CompW(AR_Bm,c.AR_C,box.vertices(:,IN));
         
         %Give an initial guess for the normal and tangential momenta
         PN=PNfull(IN);
         PT=cell2mat(PTfull(IN));
+        term1 = B_fM*c.dt - [hat(vA(4:6)), zeros(3); hat(vA(1:3)), hat(vA(4:6))]*B_M_B*vA*c.dt;
         converged = 0;
         while converged==0
             %Discrete time dynamics: Equations of motion
-            vE = vA + B_M_B\(B_fM*dt - [hat(vA(4:6)), zeros(3); hat(vA(1:3)), hat(vA(4:6))]*B_M_B*vA*dt + WNM*PN + WTM*PT);
+            vE = vA + B_M_B\(term1 + WNM*PN + WTM*PT);
             
             %Define the normal velocities at tA and tM
-            gammaNA = WNA'*vA;
-            gammaNE = WNM'*vE;
+            gammaNA = WNA'*vA-repmat(c.Cv_AC(3,1),length(IN),1);
+            gammaNE = WNM'*vE-repmat(c.Cv_AC(3,1),length(IN),1);
             
             %Define the tangential velocities at tA and tM
-            gammaTA = WTA'*vA;
-            gammaTE = WTM'*vE;
+            gammaTA = WTA'*vA-repmat(c.Cv_AC(1:2,1),length(IN),1);
+            gammaTE = WTM'*vE-repmat(c.Cv_AC(1:2,1),length(IN),1);
             
             %Newtons restitution law
-            xiN = gammaNE+eN*gammaNA;
-            xiT = gammaTE+eT*gammaTA;
+            xiN = gammaNE+c.eN*gammaNA;
+            xiT = gammaTE+c.eT*gammaTA;
             
             %Using prox functions: project PN and PT
             PNold = PN;
             PTold = PT;
-            PN = proxCN(PN-a*xiN);
-            PT = proxCT(PT-a*xiT,mu*PN);
+            PN = proxCN(PN-c.a*xiN);
+            PT = proxCT(PT-c.a*xiT,c.mu*PN);
             
             %Compute the error
             error = norm(PN-PNold)+norm(PT-PTold);
             
             %Check for convergence
-            converged = error<tol;
+            converged = error<c.tol;
         end
         BV_AB(:,ii+1) = vE;
     else
         %Update the velocity to the next time step using configuration at tM
-        vE = B_M_B\(B_fM*dt - [hat(vA(4:6)), zeros(3); hat(vA(1:3)), hat(vA(4:6))]*B_M_B*vA*dt) + vA;
+        vE = B_M_B\(B_fM*c.dt - [hat(vA(4:6)), zeros(3); hat(vA(1:3)), hat(vA(4:6))]*B_M_B*vA*c.dt) + vA;
         BV_AB(:,ii+1) = vE;
         PN = 0;
         PT = [0;0];
@@ -175,19 +143,51 @@ while run
     end
     
     %Complete the time step
-    AH_B{ii+1}  = AH_Bm*expm(0.5*dt*hat(BV_AB(:,ii+1)));
+    AH_B{ii+1}  = AH_Bm*expm(0.5*c.dt*hat(BV_AB(:,ii+1)));
     
     %Kinetic energy
     E(ii) = 0.5*BV_AB(:,ii)'*B_M_B*BV_AB(:,ii);  %Kinetic energy
     
     FN(1:length(PN),ii) = PN;
     FT(1:length(PT),ii) = PT;
-    
-    %Determine if box is at rest pose
-    if  ii > endframe
-       run = false;
-    end
-    ii = ii+1;
 end
 
+end
+
+%% Matrix with force directions
+function [WN,WT] = CompW(AR_B,AR_C,vertices)
+% Compute the matrix containing the tangential force directions.
+tel = 1;
+for ii = 1:length(vertices(1,:))
+    w = (AR_C'*[AR_B -AR_B*hat(vertices(:,ii))])';
+    WN(:,ii) = w(:,3);
+    WT(:,tel:tel+1) = w(:,1:2);
+    tel = tel+2;
+end
+end
+
+%% Proximal point Normal
+function y=proxCN(x)
+% Proximal point formulation for CN. See thesis for reference.
+% prox_CN(x) = 0 for x=< 0
+%            = x for x > 0
+y=max(x,0);
+end
+
+%% Proximal point Tangential
+function y=proxCT(x,a)
+% Proximal point formulation for CT. See thesis for reference.
+%
+% prox_CT(x) = x           for ||x|| =< a
+%            = a*(x/||x||) for ||x||  > a
+% for CT = {x in R^n| ||x|| =< a}
+cnt = 1;
+for ii = 1:length(a) %For each point in contact
+    if norm(x(cnt:cnt+1)) <= a(ii)
+        y(cnt:cnt+1,1) = x(cnt:cnt+1); %Stick
+    else
+        y(cnt:cnt+1,1) = a(ii)*x(cnt:cnt+1)/norm(x(cnt:cnt+1)); %Slip
+    end
+    cnt = cnt+2;
+end
 end
--- main.m
+++ main.m
@@ -6,41 +6,47 @@ clearvars; close all; clc; warning ('off','all'); addpath('Functions');
 dosave             = false;         %Save the trajectory (AH_B) to a .mat file
 doPlot             = true;          %Show the trajectory of the box
 %% Parameters for input
-releaseOrientation = Rx(0);         %Release orientation of the box            [deg]
-releasePosition    = [0; 0; 0.0251];%Release position of the box               [m]
-releaseLinVel      = [0; 0; 0];     %Release linear velocity (expressed in B)  [m/s]
-releaseAngVel      = [0; 0; 0];     %Release angular velocity (expressed in B) [rad/s]
-eN                 = 0.4;           %Normal coefficient of restitution         [-]
-eT                 = 0.4;           %Tangential coefficient of restitution     [-]
-mu                 = 0.4;           %Coefficient of friction                   [-]
-l                  = 0.1;           %Length of the box                         [m]
-w                  = 0.15;          %Width of the box                          [m]
-h                  = 0.05;          %Height of the box                         [m]
-m                  = 1;             %Mass of the box                           [kg]  
-AR_C               = Rx(0);         %Orientation of the contact plane          [deg]
-Ao_C               = [0; 0.6; 0];   %Position of the contact plane             [m]
-runtime            = 2;             %Runtime of the simulation                 [s]
-dt                 = 1/1000;        %Timestep at which the simulator runs      [s]
+x.releaseOrientation = Rx(0);         %Release orientation of the box            [deg]
+x.releasePosition    = [0; 0; 0.3];   %Release position of the box               [m]
+x.releaseLinVel      = [0; 0; 0];     %Release linear velocity (expressed in B)  [m/s]
+x.releaseAngVel      = [0; 0; 0];     %Release angular velocity (expressed in B) [rad/s]
+c.eN                 = 0.4;           %Normal coefficient of restitution         [-]
+c.eT                 = 0.0;           %Tangential coefficient of restitution     [-]
+c.mu                 = 0.6;           %Coefficient of friction                   [-]
+l                    = 0.1;           %Length of the box                         [m]
+w                    = 0.15;          %Width of the box                          [m]
+h                    = 0.05;          %Height of the box                         [m]
+c.a                  = 0.01;          %Prox point auxilary parameter             [-]
+c.tol                = 1e-9;          %Error tol for fixed-point                 [-]
+c.m                  = 1;             %Mass of the box                           [kg]  
+c.AR_C               = Rx(0);         %Orientation of the contact plane          [deg]
+c.Ao_C               = [0; 0.6; 0];   %Position of the contact plane             [m]
+c.Cv_AC              = [0; 0; 0];     %Linear velocity of the contact plane      [m/s]
+c.endtime            = 1.5;           %Runtime of the simulation                 [s]
+c.dt                 = 1/1000;        %Timestep at which the simulator runs      [s]
 
 %% Create the box struct
 %Mass matrix of the box
-Ml = m*eye(3);
+Ml = c.m*eye(3);
 
 %Inertia matrix of the box
-I  = [(m/12)*(w^2+h^2),                 0,                  0;
-                     0,  (m/12)*(l^2+h^2),                  0;
-                     0,                 0,   (m/12)*(l^2+w^2);];
+I  = [(c.m/12)*(w^2+h^2),                 0,                  0;
+                     0,  (c.m/12)*(l^2+h^2),                  0;
+                     0,                 0,   (c.m/12)*(l^2+w^2);];
 %Inertia tensor
 box.B_M_B = [Ml zeros(3,3); zeros(3,3) I];
 
-%Vertices
-box.vertices = BoxVertices(l,w,h); 
+%Vertices of the box expressed in body fixed frame B (frame B at COM)
+box.vertices(:,1) = [ l/2; -w/2; -h/2];      box.vertices(:,5) = [ l/2; -w/2;  h/2];
+box.vertices(:,2) = [-l/2; -w/2; -h/2];      box.vertices(:,6) = [-l/2; -w/2;  h/2];
+box.vertices(:,3) = [-l/2;  w/2; -h/2];      box.vertices(:,7) = [-l/2;  w/2;  h/2];
+box.vertices(:,4) = [ l/2;  w/2; -h/2];      box.vertices(:,8) = [ l/2;  w/2;  h/2];
 
 %Mass
-box.mass = m;
+box.mass = c.m;
 
 %% Run the dynamics
-[AH_B,BV_AB,FN,FT] = BoxSimulator(releasePosition,releaseOrientation,releaseLinVel,releaseAngVel,eN,eT,mu,box,AR_C,Ao_C,dt,runtime);
+[AH_B,BV_AB,FN,FT] = BoxSimulator(x,c,box);
 
 restPosition = AH_B{end}(1:3,4);      %Rest position of the box
 restOrientation = AH_B{end}(1:3,1:3); %Rest orientation of the box
@@ -51,17 +57,39 @@ set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultAxesTickLabelInterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 
+%Extract some values
+AR_C=c.AR_C;
+Ao_C=c.Ao_C;
+dt =c.dt;
+runtime=c.endtime;
+
+
 % Plotting options 
 %For plotting the contact surface
 ws    = 1;                 %With of the contact surface             [m]
 ls    = 1.6;               %Length of the contact surface           [m]
 surfacepoints = [0.5*ws -0.5*ws -0.5*ws 0.5*ws 0.5*ws; -0.5*ls -0.5*ls 0.5*ls 0.5*ls -0.5*ls; 0 0 0 0 0;];
 spoints = AR_C*surfacepoints +Ao_C; %Transform the vertices according to position/orientation of the surface
+step = 25; %Number of discrete points we skip per shown frame
+
+%Vector field
+X = linspace(-20,20,205);
+Y = linspace(-20,20,205);
+Z = 0;
+tel = 1;
+vecveltemp =[];
+for xx =1:length(X)
+    for yy = 1:length(Y)
+        vecveltemp(:,tel) = [X(xx);Y(yy);Z]';
+        tel=tel+1;
+    end
+end
+
 
 %Plot the trajectory of the box
 if doPlot
     figure;
-    for ii=1:15:length(AH_B)
+    for ii=1:step:length(AH_B)
         plotBox(AH_B{ii},box,[0 0 1]);
         
         
@@ -80,6 +108,17 @@ if doPlot
         plot3([0 tip(1,1)],[0 tip(2,1)],[0 tip(3,1)],'r'); hold on
         plot3([0 tip(1,2)],[0 tip(2,2)],[0 tip(3,2)],'g');
         plot3([0 tip(1,3)],[0 tip(2,3)],[0 tip(3,3)],'b');
+
+        %Draw the velocity vector of the
+%         plot3(vecvel(1,:),vecvel(2,:),vecvel(3,:),'o'); hold on;
+%         annotation('textarrow',[vecvel(1,:)' vecvel2(1,:)'],[vecvel(2,:)' vecvel2(2,:)'],[vecvel(3,:)' vecvel2(3,:)'])
+
+        vecvel = AR_C*(vecveltemp+c.Cv_AC*(dt*(ii-1)))+Ao_C;
+        speed = c.Cv_AC/norm(c.Cv_AC);
+        vecvel2 = AR_C*repmat(0.15*speed,1,length(vecvel));
+        pbool = (vecvel(1,:)>=(min(spoints(1,:))))&(vecvel(1,:)<=(max(spoints(1,:))))&(vecvel(2,:)>=(min(spoints(2,:))))&(vecvel(2,:)<=(max(spoints(2,:))))&(vecvel(3,:)>=(min(spoints(3,:))))&(vecvel(3,:)<=(max(spoints(3,:))));
+        quiver3(vecvel(1,pbool),vecvel(2,pbool),vecvel(3,pbool),vecvel2(1,pbool),vecvel2(2,pbool),vecvel2(3,pbool),'off');
+
         
         
         grid on;axis equal;
@@ -97,53 +136,54 @@ if dosave ==1
 end
 
 
-% Plot Position, Velocity, Force
-AH_Bm = cat(3,AH_B{:});
-set(groot,'defaulttextinterpreter','latex'); set(groot,'defaultAxesTickLabelInterpreter','latex'); set(groot,'defaultLegendInterpreter','latex');
-% close all;
-
-figure; 
-plot(0:dt:runtime,flipud(FN)');
-% axis([0 10 0.002451 0.002454]);
-grid on;
-legend('Contact point 1','Contact point 2','Contact point 3','Contact point 4');
-ylabel('$F_N$ [N]');
-xlabel('time [s]');
-title('Normal force of the contact points for a box of 1 kg');
-
-figure;
-sgtitle('Velocity of the COM of the box in x,y,z-direction');
-subplot(1,3,1);
-plot(0:dt:runtime,BV_AB(1,1:end-1));
-ylabel('Velocity [m/s]');
-xlabel('Time [s]');
-title('x');
-
-subplot(1,3,2);
-plot(0:dt:runtime,BV_AB(2,1:end-1));
-xlabel('Time [s]');
-title('y');
-
-subplot(1,3,3);
-plot(0:dt:runtime,BV_AB(3,1:end-1));
-xlabel('Time [s]');
-title('z');
-ylim([-8e-6 8e-6]);
-
-figure;
-sgtitle('Position of the COM of the box in x,y,z-direction');
-subplot(1,3,1);
-plot(0:dt:runtime,squeeze(AH_Bm(1,4,1:end-1)));
-title('x');
-xlabel('Time [s]');
-ylabel('Position [m]');
-
-subplot(1,3,2);
-plot(0:dt:runtime,squeeze(AH_Bm(2,4,1:end-1)))
-title('y');
-xlabel('Time [s]');
-
-subplot(1,3,3);
-plot(0:dt:runtime,squeeze(AH_Bm(3,4,1:end-1)))
-title('z');
-xlabel('Time [s]');
+% % Plot Position, Velocity, Force
+% AH_Bm = cat(3,AH_B{:});
+% set(groot,'defaulttextinterpreter','latex'); set(groot,'defaultAxesTickLabelInterpreter','latex'); set(groot,'defaultLegendInterpreter','latex');
+% % close all;
+% time=0:dt:runtime;
+% 
+% figure; 
+% plot(time(1:end-1),flipud(FN)');
+% % axis([0 10 0.002451 0.002454]);
+% grid on;
+% legend('Contact point 1','Contact point 2','Contact point 3','Contact point 4');
+% ylabel('$F_N$ [N]');
+% xlabel('time [s]');
+% title('Normal force of the contact points for a box of 1 kg');
+% 
+% figure;
+% sgtitle('Velocity of the COM of the box in x,y,z-direction');
+% subplot(1,3,1);
+% plot(time(1:end-1),BV_AB(1,1:end-1));
+% ylabel('Velocity [m/s]');
+% xlabel('Time [s]');
+% title('x');
+% 
+% subplot(1,3,2);
+% plot(time(1:end-1),BV_AB(2,1:end-1));
+% xlabel('Time [s]');
+% title('y');
+% 
+% subplot(1,3,3);
+% plot(time(1:end-1),BV_AB(3,1:end-1));
+% xlabel('Time [s]');
+% title('z');
+% ylim([-8e-6 8e-6]);
+% 
+% figure;
+% sgtitle('Position of the COM of the box in x,y,z-direction');
+% subplot(1,3,1);
+% plot(time(1:end-1),squeeze(AH_Bm(1,4,1:end-1)));
+% title('x');
+% xlabel('Time [s]');
+% ylabel('Position [m]');
+% 
+% subplot(1,3,2);
+% plot(time(1:end-1),squeeze(AH_Bm(2,4,1:end-1)))
+% title('y');
+% xlabel('Time [s]');
+% 
+% subplot(1,3,3);
+% plot(time(1:end-1),squeeze(AH_Bm(3,4,1:end-1)))
+% title('z');
+% xlabel('Time [s]');
